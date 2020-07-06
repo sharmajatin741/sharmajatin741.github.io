7ffd5ad315e10127b014125987f9f262c50a729f
sharmajatin741 <sharmajatin741@gmail.com>
2020-07-06 20:32:20 +0530

    Updates

---
+++ precache-manifest.cc2990df2661fbd9fbe0e53854868031.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca0a4fbdaf3d14df719fa376c35119f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91ae1d48a32a58e5932",
+    "url": "/static/css/main.45071fe7.chunk.css"
+  },
+  {
+    "revision": "b5c7e8e4f987550fc849",
+    "url": "/static/js/2.2b487fad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b487fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91ae1d48a32a58e5932",
+    "url": "/static/js/main.3663ede1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "d8086b399ea563216867ce70e422ecd4",
+    "url": "/static/media/cc.d8086b39.png"
+  },
+  {
+    "revision": "c37c9c07b6a62a506a82e7fa9f543a56",
+    "url": "/static/media/github.c37c9c07.png"
+  },
+  {
+    "revision": "4cd7f43818621e24eaedaa3d9c3aa675",
+    "url": "/static/media/hackerearth.4cd7f438.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92594f05b8e5bfb174e071970c256d86",
+    "url": "/static/media/mypic.92594f05.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4a89a7ae9b941166d6d519c94ebf9db.js"
+  "/precache-manifest.cc2990df2661fbd9fbe0e53854868031.js"
 );
 
 self.addEventListener('message', (event) => {
