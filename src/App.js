49cb78e56e2b1779760681bb9eb657c60cc178c0
sharmajatin741 <sharmajatin741@gmail.com>
2020-07-06 20:29:21 +0530

    made website in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
+import mypic from './images/mypic.jpg'
+import React, { Component } from 'react'
+import codeforces from './images/codeforces.png';
+import email from './images/email.png';
+import github from './images/github.png';
+import linkedin from './images/linkedin.png';
+import resume from './images/resume.png';
+import hackerearth from './images/hackerearth.png'
+import cc from './images/cc.png'
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mypic} className="mypiccss" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jatin Sharma
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <div>
+        <a href="https://github.com/sharmajatin741" title="Github">
+        <img src={github} className="cfcss" alt="logo" />
         </a>
+        <a href="mailto:sharmajatin741@gmail.com" title="Email">
+        <img src={email} className="cfcss" alt="logo" />
+        </a>
+        <a href="https://codeforces.com/profile/sharmajatin741" title="Codeforces">
+        <img src={codeforces} className="cfcss" alt="logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/sharmajatin741/" title="Linkedin">
+        <img src={linkedin} className="cfcss" alt="logo" />
+        </a>
+        <a href="https://www.hackerearth.com/@sharmajatin741" title="Hackerearth">
+        <img src={hackerearth} className="cfcss" alt="logo" />
+        </a>
+        <a href="https://www.codechef.com/users/sharmajatin741" title="Codechef">
+        <img src={cc} className="cfcss" alt="logo" />
+        </a>
+        </div>
       </header>
     </div>
-  );
+    </div>
+    )
+  }
 }
 
 export default App;
